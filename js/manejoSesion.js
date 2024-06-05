bf0eecc7f6361265e5ead8c104e42cec9ff325d0
Lucas Cristin <lukicristin@gmail.com>
2024-06-05 00:23:57 -0300

    Una ves enviado el form se redirige al usuario donde se consume api

--- js/manejoSesion.js
+++ js/manejoSesion.js
@@ -1,13 +1,25 @@
-function validateForm() {
+function validateForm(event) {
+    
+    event.preventDefault()
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
     var error = document.getElementById("error");
 
     if (email === "" || password === "") {
+        
         error.style.display = "block";
         return false;
     } else {
         error.style.display = "none";
+        redirigir();
         return true;
     }
+  
+}
+
+function redirigir()
+{
+    var meta = document.querySelector("#redirect");
+    meta.setAttribute("http-equiv", "refresh");
+    meta.setAttribute("content","2;url=./apiConexiones.html");
 }
